cfd107214378140cc727d432c8cae5488ce631fb
Cintia Elisa <cintiaelisa@gmail.com>
2021-05-07 09:25:03 -0300

    Basic static components

---
+++ frontend/src/components/footer/index.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <div>
+      <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+          <p className="text-light">App desenvolvido por <a href="https://github.com/cintiaelisa" target="_blank" rel="noreferrer">Cintia Elisa</a></p>
+          <p className="text-light"><small><strong>Semana Spring React</strong></small></p>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
